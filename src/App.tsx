825be1cedd75dc6e2d3acc3b9768fdf3d98253e5
GensIto <genta1121@icloud.com>
2023-03-09 20:53:58 +0900

    remove supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import CssBaseline from '@mui/material/CssBaseline'
-import './App.css'
 import { Button } from '@mui/material'
 
 const App: React.FC = () => {
@@ -10,34 +8,15 @@ const App: React.FC = () => {
   return (
     <>
       <CssBaseline />
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <Button
-            variant="contained"
-            sx={style.button1}
-            onClick={() => {
-              setCount((count) => count + 1)
-            }}
-          >
-            count is {count}
-          </Button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <Button
+        variant="contained"
+        sx={style.button1}
+        onClick={() => {
+          setCount((count) => count + 1)
+        }}
+      >
+        count is {count}
+      </Button>
     </>
   )
 }
